6a65988d89ef0ff621bc9387165dac050a3bf6bd
hqwu <464693894@qq.com>
2013-12-19 10:31:52 +0800

    add encode

--- MasterStation/src/com/station/md5/MD5.java
+++ MasterStation/src/com/station/md5/MD5.java
@@ -1,18 +1,23 @@
 package com.station.md5;
 
+import java.io.UnsupportedEncodingException;
+
 
 public class MD5 {
 	public static String CreateMD5String(String str){
 		try {
 	        java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
-	        byte[] array = md.digest(str.getBytes());
+	        byte[] array = md.digest(str.getBytes("utf-8"));
 	        StringBuffer sb = new StringBuffer();
 	        for (int i = 0; i < array.length; ++i) {
 	          sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1,3));
 	       }
 	        return sb.toString();
 	    } catch (java.security.NoSuchAlgorithmException e) {
-	    }
+	    } catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return null;
 	}
 }
